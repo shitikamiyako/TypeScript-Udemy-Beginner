c147c1bc0f8c2d5c66c0aea86ac967f7fa4b1644
shitikakei <shitikakei@gmail.com>
2021-08-28 22:35:24 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// 型推論<型定義(アノテーション)の優先順位
+
+const name = "hello";
+let nameChange = "hello1";
+
+let array1 = [true, false];
+let array2 = [true, false, 1, 'string'];
+
+interface NAME {
+  first: string;
+  // interfaceでプロパティに?をつけると以後作るObj等で必須ではなくなる
+  second?: string;
+  last: string;
+
+}
+
+// secondプロパティはなくても問題ない
+let nameObj:NAME = { first: 'mio', last: 'kamina'};
+
+const func = (x:number, y:number):number => {
+  return x + y;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
